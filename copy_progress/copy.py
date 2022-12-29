4bdc4719b1dcf6412854041d56ae09f6189b7694
Caelan Barondess <cbarondess19@gmail.com>
2022-12-28 17:34:23 -0700

    Update functions

--- copy_progress/copy.py
+++ copy_progress/copy.py
@@ -8,14 +8,16 @@ import time
 
 from rich.progress import Progress, TransferSpeedColumn, FileSizeColumn, TotalFileSizeColumn
 
-progress = Progress(
-    *Progress.get_default_columns(),
-    TransferSpeedColumn(),
-    FileSizeColumn(),
-    TotalFileSizeColumn()
-)
+def setup():
+    progress_bar = Progress(
+        *Progress.get_default_columns(),
+        TransferSpeedColumn(),
+        FileSizeColumn(),
+        TotalFileSizeColumn()
+    )
+    return progress_bar
 
-def copy_dir():
+def copy_dir(progress):
     files = os.listdir(sys.argv[1])
     print(files)
     with progress as progress:
@@ -27,7 +29,7 @@ def copy_dir():
                 with open(arg2, "wb") as dst:
                     shutil.copyfileobj(src, dst)
 
-def copy_file():
+def copy_file(progress):
     with progress as progress:
         arg1 = sys.argv[1]
         arg2 = sys.argv[2]
@@ -37,41 +39,15 @@ def copy_file():
                 shutil.copyfileobj(src, dst)
     
 def main():
+    progress_bar = setup()
     if len(sys.argv) == 3:
         if os.path.isdir(sys.argv[1]):
-            copy_dir()
+            copy_dir(progress_bar)
         if os.path.isfile(sys.argv[1]):
-            copy_file()
+            copy_file(progress_bar)
     else:
         print("Copy a file with a progress bar.")
-        print("Usage:\n\tpython cp_progress.py SRC DST")
+        print("Usage:\n\tcopy-progress SRC DST")
 
 if __name__ == "__main__":
     main()
-
-""" 
-if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        with progress as progress:
-            desc = os.path.basename(sys.argv[1])
-            shutil.copytree(sys.argv[1], sys.argv[2], dirs_exist_ok=True)
-    else:
-        print("Copy a file with a progress bar.")
-        print("Usage:\n\tpython cp_progress.py SRC DST")
- """
-
-""" if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        files = os.listdir(sys.argv[1])
-        print(files)
-        for item in files:
-            arg1 = os.path.join(sys.argv[1], item)
-            arg2 = os.path.join(sys.argv[2], item)
-            with progress as progress:
-                desc = os.path.basename(arg1)
-                print(f'{arg1}\t{arg2}')
-                #with progress.open(arg1, "rb", description=desc) as src:
-                #    with open(arg2, "wb") as dst:
-    else:
-        print("Copy a file with a progress bar.")
-        print("Usage:\n\tpython cp_progress.py SRC DST") """
